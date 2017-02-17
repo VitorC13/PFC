bde10dba74a1ac7a4400ca3d776eba2937927dcd
Vitor Cardoso <vitorcardoso.desenvolvedor@gmail.com>
2017-02-16 22:24:39 -0200

    Conflict
    Resolvido

---
+++ src/controller/AgendamentoController.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import dao.AgendamentoDAO;
+import dao.PacientesDAO;
+import dao.VacinasDAO;
+import java.sql.SQLException;
+import java.text.ParseException;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import model.Agendamento;
+import model.Paciente;
+import model.Vacinas;
+
+/**
+ *
+ * @author Kanec
+ */
+public class AgendamentoController {
+
+    public boolean inserirAgendamento(Vacinas v, Paciente p, Agendamento a) throws SQLException, ParseException {
+
+        AgendamentoDAO dao = new AgendamentoDAO();
+        PacientesDAO daoPac = new PacientesDAO();
+        VacinasDAO daoVac = new VacinasDAO();
+        final JPanel panel = new JPanel();
+        if (a.getDataDose() != null && v.getId() == daoVac.setID(v)
+                && v.isAtivo() != daoVac.setAtivo(v) && p.getId() == daoPac.setID(p)
+                && p.isAtivo() != daoPac.setAtivo(p)) {
+            a.setDataDose(a.getDataDose());
+            p.setId(p.getId());
+            v.setId(v.getId());
+            a.setPaciente(p);
+            a.setVacinas(v);
+            dao.cadastrarNovosAgendamentos(a);
+            JOptionPane.showMessageDialog(panel, "Agendamento Cadastrado", "Warning", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(panel, "Inserção Incorreta de Dados", "Warning", JOptionPane.WARNING_MESSAGE);
+        }
+
+        return true;
+    }
+
+    public boolean atualizarAgendamentoDia(Agendamento a) throws SQLException {
+
+        AgendamentoDAO dao = new AgendamentoDAO();
+
+        final JPanel panel = new JPanel();
+        if (a.getDataDose() != null && a.getId() == dao.selectID(a)
+                && a.isAtivo() != dao.setAtivo(a)) {
+            a.setDataDose(a.getDataDose());
+            dao.atualizarAgendamentoDia(a);
+            JOptionPane.showMessageDialog(panel, "Dia do Agendamento Atualizado", "Warning", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(panel, "Inserção Incorreta de Dados", "Warning", JOptionPane.WARNING_MESSAGE);
+        }
+
+        return true;
+    }
+
+    public boolean atualizarAgendamentoVacina(Vacinas v, Agendamento a) throws SQLException {
+
+        AgendamentoDAO dao = new AgendamentoDAO();
+        VacinasDAO daoVac = new VacinasDAO();
+
+        final JPanel panel = new JPanel();
+        if (a.getDataDose() != null && a.getId() == dao.selectID(a)
+                && a.isAtivo() != dao.setAtivo(a) && v.getId() == daoVac.setID(v)
+                && v.isAtivo() != daoVac.setAtivo(v)) {
+            a.setDataDose(a.getDataDose());
+            v.setId(v.getId());
+            dao.atualizarAgendamentoVacina(v, a);
+            JOptionPane.showMessageDialog(panel, "Vacina do Dia de Agendamento Atualizada", "Warning", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(panel, "Inserção Incorreta de Dados", "Warning", JOptionPane.WARNING_MESSAGE);
+        }
+
+        return true;
+    }
+
+    public boolean excluirAgendamento(Agendamento a) throws SQLException {
+
+        AgendamentoDAO dao = new AgendamentoDAO();
+        final JPanel panel = new JPanel();
+        if (a.getId() == dao.selectID(a) && a.isAtivo() != dao.setAtivo(a)) {
+            a.setId(a.getId());
+            dao.excluirAgendamento(a);
+            JOptionPane.showMessageDialog(panel, "Agendamento Apagado", "Warning", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(panel, "ID não achado", "Warning", JOptionPane.WARNING_MESSAGE);
+        }
+        return true;
+    }
+
+    public boolean mostrarAgendamento(Agendamento a) throws SQLException {
+
+        AgendamentoDAO dao = new AgendamentoDAO();
+        final JPanel panel = new JPanel();
+        if (a.getId() == dao.selectID(a) && a.isAtivo() != dao.setAtivo(a)) {
+            a.setId(a.getId());
+            dao.buscarAgendamento(a);
+        } else {
+            JOptionPane.showMessageDialog(panel, "ID não achado", "Warning", JOptionPane.WARNING_MESSAGE);
+        }
+        return true;
+    }
+}
